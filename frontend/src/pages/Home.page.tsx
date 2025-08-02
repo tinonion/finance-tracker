9161c95d3e648e4280f850de4d6fedfa56e1b6a8
Hunter Jones <whjones@desktop-cent9.attlocal.net>
2025-08-02 12:15:28 -0400

    basic NavLink bar

--- frontend/src/pages/Home.page.tsx
+++ frontend/src/pages/Home.page.tsx
@@ -1,11 +1,36 @@
+import { TableOfContents } from '@mantine/core';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import { useState } from 'react';
+import { IconListDetails } from '@tabler/icons-react';
+import { Box, NavLink } from '@mantine/core';
+
+const data = [
+  { icon: IconListDetails, label: 'Expense Tracker' },
+];
 
 export function HomePage() {
+  const [active, setActive] = useState(0);
+
+  const items = data.map((item, index) => (
+    <NavLink
+      styles={{
+        root: {},
+        section: {marginRight: "7px"},
+        label: { fontSize: 18 }
+      }}
+      href="#required-for-focus"
+      key={item.label}
+      active={index === active}
+      label={item.label}
+      leftSection={<item.icon size={24} stroke={2} />}
+      onClick={() => setActive(index)}
+    />
+  ));
+
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Box style={{height: "100%"}} w={300} h="100%">{items}</Box>
     </>
   );
 }
